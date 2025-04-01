ce7fd2fa2d95c65c6fd0b6a55d76db72c07c7677
ayomi-20 <peaceayomirwoth@gmail.com>
2025-04-01 13:54:59 +0300

    added button and pop up modal

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const unreadList = document.getElementById("unreadList");
 const readList = document.getElementById("readList");
 const modal = document.getElementById("addbook");
 
-// opening the modal when the button is clicked
-document.getElementById("openModalBtn").addEventListener("click", () => {
-    var modalElement = new bootstrap.Modal(modal);
-    modalElement.show();
-});
+// // opening the modal when the button is clicked
+// document.getElementById("openModalBtn").addEventListener("click", () => {
+//     var modalElement = new bootstrap.Modal(modal);
+//     modalElement.show();
+// });
 
+// Selecting elements for the modal and book list
+const submitBtn = document.getElementById("submitBtn");
+const booklist = document.getElementById("bookList");
+
+// Handling form submission
+submitBtn.addEventListener("click", (event) => {
+    event.preventDefault();}) // Preventing page reload on form submit
